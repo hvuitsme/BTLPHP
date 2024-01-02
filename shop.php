133c2ba5f72f574a6ee728c3f22abf9468729518
bedanntq <ok7600480@gmail.com>
2024-01-02 22:35:38 +0700

    Add product and Database

--- shop.php
+++ shop.php
@@ -9,16 +9,11 @@
 
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&family=Lobster&family=Taviraj:wght@100;300&display=swap"
-    rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&family=Lobster&family=Taviraj:wght@100;300&display=swap" rel="stylesheet" />
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <style>
     body {
@@ -78,27 +73,61 @@
 
         <h1>SHOP</h1>
 
-        <div class="row">
-          <div class="col-sm-4">
-            <div class="card sp" style="width: 100%" onmouseover="addHoverEffect(this)"
-              onmouseout="removeHoverEffect(this)" onclick="redirectToAnotherPage()">
-              <img class="h-100"
-                src="https://lavitta.vn/cdn/shop/files/z5009401304552_6e7b6b911bc5b000603b56ca5b04a874_600x.jpg?v=1703524364"
-                alt="" />
-              <a href="#" class="eye-link">
-                <i class="fa-regular fa-eye"></i>
-              </a>
-            </div>
+        <div class="container pt_2">
+          <div class="row">
 
-            <div class="d-flex justify-content-center pt-3">
-              <p class="m-0">Đầm hồng cá tính</p>
-            </div>
-            <!-- phan nay -->
-            <div class="d-flex justify-content-center pt-3">
-              <button type="button" class="btn btn-danger">
-                Thêm vào giỏ hàng
-              </button>
-            </div>
+            <?php
+            // Kết nối đến cơ sở dữ liệu
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "web";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Kiểm tra kết nối
+            if ($conn->connect_error) {
+              die("Kết nối không thành công: " . $conn->connect_error);
+            }
+            ?>
+            <?php
+            $sql = "SELECT * FROM `image_demo_vaycuoi`";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+              // Duyệt qua từng dòng dữ liệu
+              while ($row = $result->fetch_assoc()) {
+            ?>
+                <div class="col-sm-3 mt-3">
+                  <div class="card sp" style="width: 100%" onmouseover="addHoverEffect(this)" onmouseout="removeHoverEffect(this)" onclick="redirectToAnotherPage()">
+                    <img class="h-100" src="data:image/jpeg;base64,<?= base64_encode($row['image_sp_demo']) ?>" alt="" />
+                    <a href="#" class="eye-link">
+                      <i class="fa-regular fa-eye"></i>
+                    </a>
+                  </div>
+
+                  <div class="d-flex justify-content-center pt-3">
+                    <p class="m-0 text-center">
+                      <?= $row['name_sp_demo'] ?><br>
+                      <?= $row['price'] ?><br>
+                    </p>
+                  </div>
+
+                  <div class="d-flex justify-content-center pt-1">
+                    <button type="button" class="btn btn-danger">
+                      Thêm vào giỏ hàng
+                    </button>
+                  </div>
+                </div>
+            <?php
+              }
+            } else {
+              echo "Không có dữ liệu";
+            }
+
+            // Đóng kết nối
+            $conn->close();
+            ?>
           </div>
         </div>
       </div>
@@ -108,7 +137,7 @@
   <!-- js script -->
 
   <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
       // Kiểm tra kích thước màn hình và thêm/xóa lớp CSS
       function checkScreenSize() {
         var screenSize =
@@ -125,7 +154,7 @@
       }
 
       // Gọi hàm kiểm tra khi trang web tải và khi thay đổi kích thước màn hình
-      $(window).on("load resize", function () {
+      $(window).on("load resize", function() {
         checkScreenSize();
       });
     });
@@ -154,7 +183,7 @@
     var links = document.querySelectorAll('a span');
 
     // Lặp qua từng thẻ để kiểm tra đường dẫn
-    links.forEach(function (link) {
+    links.forEach(function(link) {
       // Lấy đường dẫn từ href của thẻ a chứa thẻ span
       var linkPath = link.parentElement.getAttribute('href');
 
@@ -170,9 +199,7 @@
 
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
