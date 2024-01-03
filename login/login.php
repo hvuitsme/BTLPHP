6503fded552f93db694783c73763209e52d38fd6
hvuitsme <hvuits.@gmail.com>
2024-01-03 12:56:59 +0700

    redistribute the route and revise the login interface

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Success login</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
