7f682d37d6de6fe3b698ae232cdae337f8375e9d
hvuitsme <hvuits.@gmail.com>
2024-01-10 23:30:43 +0700

    Add add2cart

--- home/index.php
+++ home/index.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="../css/style.css" />
   <title>webvaycuoi</title>
-
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link
@@ -255,7 +254,7 @@
     <hr>
     <!-- sản phẩm -->
     <div class="container pt-2">
-      <div class="row">
+      <!-- <div class="row">
         <div class="col-sm-5">
           <img class="rounded-2" src="../img/feature-image.jpg" style="height: 200px;" alt="">
         </div>
@@ -277,7 +276,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 
@@ -402,9 +401,16 @@
             </div>
 
             <div class="d-flex justify-content-center pt-3">
-              <button type="button" class="btn btn-danger">
-                Thêm vào giỏ hàng
-              </button>
+              <form method="post" action="add2cart.php">
+                <input type="hidden" name="product_image" value="<?= base64_encode($row['image_sp']) ?>">
+                <input type="hidden" name="product_name" value="<?= $row['name_sp'] ?>">
+                <input type="hidden" name="product_code" value="<?= $row['code_sp'] ?>">
+                <input type="hidden" name="product_price" value="<?= $row['price'] ?>">
+
+                <button type="submit" name="add_to_cart" class="btn btn-danger">
+                  Thêm vào giỏ hàng
+                </button>
+              </form>
             </div>
           </div>
           <?php
@@ -681,6 +687,8 @@
     });
   </script>
 
+  <!-- test -->
+
   <!-- quantity Selector -->
   <script>
     function increaseQuantity() {
@@ -698,7 +706,6 @@
   <!-- cdnjs -->
 
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
